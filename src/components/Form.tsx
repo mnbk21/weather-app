5869f1cc0778e952c3d7b7aeaf8050bdc0677f49
mnbk21 <mnbk21@gmail.com>
2024-07-06 12:25:45 +0900

    プレースフォルダ修正

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -9,7 +9,7 @@ const Form = (props: FormProps) => {
 
   return (
     <form onSubmit={props.getWeather}>
-      <input type="text" name="city" placeholder="都市名" onChange={event => props.setCity(event.target.value)}/>
+      <input type="text" name="city" placeholder="都市名を英語で入力" onChange={event => props.setCity(event.target.value)}/>
 
       <button type="submit">Get weather</button>
     
